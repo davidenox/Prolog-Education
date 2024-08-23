72edc15738c1e94c0adb6e6c99f9e81a3ca93477
davidenox <nocedav31@gmail.com>
2024-08-24 00:50:34 +0200

    Starting real education

---
+++ LearnPrologNow/KnownledgeBase.pl
@@ -0,0 +1,50 @@
+% KB1
+
+% semplice collezione di fatti
+woman(mia).
+woman(jody).
+woman(yolanda).
+playsAirGuitar(jody).
+party.
+
+% KB2
+
+happy(yolanda).
+listens2Music(mia).
+listens2Music(yolanda):- happy(yolanda). % se yolanda è felice allora ascolta musica
+playsAirGuitar(mia):- listens2Music(mia).
+playsAirGuitar(yolanda):- listens2Music(yolanda).
+% il simbolo :- è un operatore logico da leggere come "se" oppure "deriva da", ed è una regola di inferenza
+
+% KB3
+
+happy(vincent).
+listens2Music(butch).
+playsAirGuitar(vincent):-
+    listens2Music(vincent),
+    happy(vincent).
+% rappresento una AND mettendo i due goal nella stessa regola
+playsAirGuitar(butch):-
+    happy(butch).
+playsAirGuitar(butch):-
+    listens2Music(butch).
+% rappresento una OR scrivendoli separatamente, dichiarando che butch suona se è felice oppure se sente musica
+
+% KB4
+
+woman(mia).
+woman(jody).
+woman(yolanda).
+
+loves(vincent,mia).
+loves(marsellus,mia).
+loves(pumpkin,honey_bunny).
+loves(honey_bunny,pumpkin).
+
+%come se indicasse la relazione vincent-loves-mia
+%se chiedo ?-loves(X,mia). avrò come risultato vincent e marsellus
+%identifico X come l'incognita da trovare come richiesta
+
+% KB5
+
+
