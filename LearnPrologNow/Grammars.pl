2f63d5bf522a6a508fdda605159027ab45eeb008
davidenox <nocedav31@gmail.com>
2024-08-30 18:09:26 +0200

    in-depth study on grammars

--- LearnPrologNow/Grammars.pl
+++ LearnPrologNow/Grammars.pl
@@ -32,6 +32,7 @@ append([],L,L).
 append([H|T],L2,[H|L3]) :- 
     append(T,L2,L3).
 
+
 s(Z) :- np(X), vp(X), append(X,Y,Z).
 
 np(Z) :- det(X), n(Y), append(X,Y,Z).
@@ -48,4 +49,78 @@ n([man]).
 
 v([shoots]).
 
+/* Definite Clause Grammars (DCG)*/
+
+s --> np, vp.
+
+np --> det, n.
+
+vp --> v, np.
+vp --> v.
+
+det --> [the].
+det --> [a].
+
+n --> [woman].
+n --> [man].
+
+v --> [shoots].
+
+/*Si tratta della notazione il più comprensibile possibile da un umano*/
+
+/*Utilizzando 
+ * 
+ * ?-listing(s) si avrà:
+ * 
+ * s(A, B) :-
+    np(A, C),
+    vp(C, B).
+ * */
+
+/* Regole ricorsive */
+
+s --> s,conj,s.
+
+conj --> [and].
+conj --> [or].
+conj --> [but].
+
+/* Ecco cosa succede: 
+s(A, B) :-
+    np(A, C),
+    vp(C, B).
+s(A, B) :-
+    s(A, C),
+    conj(C, D),
+    s(D, B).
+ * 
+ * */
+
+/* È utile riscrivere in maniera più organizzata il codice: */
+
+s --> simple_s.
+s --> simple_s,conj,s.
+simple_s --> np,vp.
+np --> det,n.
+vp --> v,np.
+vp --> v.
+det --> [the].
+det --> [a].
+n --> [woman].
+n --> [man].
+v --> [shoots].
+conj --> [and].
+conj --> [or].
+conj --> [but].
+
+/* Tramite tutte queste regole è possibile usare le grammatiche per creare dei linguaggi
+ * semplici ma formali, ad esempio: */
+
+t --> [].
+t --> l,t,r.
+
+l --> [a].
+r --> [b].
+
+/* Extra */
 
