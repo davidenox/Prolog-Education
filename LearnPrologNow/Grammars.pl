0cb735029a3eb9dfa2fea483fab3ccaa0745df90
davidenox <nocedav31@gmail.com>
2024-09-02 21:30:18 +0200

    Extra for DCGs

--- LearnPrologNow/Grammars.pl
+++ LearnPrologNow/Grammars.pl
@@ -124,3 +124,105 @@ r --> [b].
 
 /* Extra */
 
+/* Questa è la DCG su cui abbiamo lavorato fino ad ora:*/
+
+s --> np,vp.
+
+np --> det,n.
+
+vp --> v,np.
+vp --> v.
+
+det --> [the].
+det --> [a].
+
+n --> [woman].
+n --> [man].
+
+v --> [shoots].
+
+/* Se volessimo aggiungere frasi come "She shoots him" o "He shoots her", potremmo
+ * aggiungere la regola dei pronomi:*/
+
+pro --> [he].
+pro --> [she].
+pro --> [him].
+pro --> [her].
+
+np --> pro.
+
+/* Queste regole non sono complete, poiché la grammatica accetta delle frasi che sono
+ * sintatticamente errate. Si può riscrivere in maniera migliore: */
+
+s --> np_subject,vp.
+
+np_subject --> det,n.
+np_object  --> det,n.
+np_subject --> pro_subject.
+np_object  --> pro_object.
+
+vp --> v,np_object.
+vp --> v.
+
+det --> [the].
+det --> [a].
+
+n --> [woman].
+n --> [man].
+
+pro_subject --> [he].
+pro_subject --> [she].
+pro_object --> [him].
+pro_object --> [her].
+
+v --> [shoots].
+
+/* Questa versione non è ancora corretta, potrebbe essere migliorata: */
+
+s --> np(subject),vp.
+
+np(_) --> det,n.
+np(X) --> pro(X).
+
+vp --> v,np(object).
+vp --> v.
+
+det --> [the].
+det --> [a].
+
+n --> [woman].
+n --> [man].
+
+pro(subject) --> [he].
+pro(subject) --> [she].
+pro(object) --> [him].
+pro(object) --> [her].
+
+v --> [shoots].
+
+/* Costruire un Parse-Tree */
+
+/* Utilizzando solo gli strumenti standard di Prolog non possiamo effettivamente 
+ * disegnare belle immagini di alberi, ma possiamo costruire strutture dati che 
+ * descrivono gli alberi in modo chiaro. Ad esempio, corrispondente all'albero 
+ * s np det a n woman vp v shoots 
+ * possiamo avere la seguente terminologia:
+ * ?- s(np(det(a),n(woman)),vp(v(shoots))).
+ * Non è di certo bello da vedere, ma l'informazione è racchiusa in questa query
+ * 
+ * Una DCG deve
+ * 
+ * */
+
+
+
+
+
+
+
+
+
+
+
+
+
